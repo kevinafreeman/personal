79c3b2c1a2c37d4aaa85703d66f2608bfc47de1c
Kevin Freeman <kfreem2@g.clemson.edu>
2014-10-08 20:08:45 -0400

    pushing changes and edits

--- scripts/installscript.sh
+++ scripts/installscript.sh
@@ -7,10 +7,11 @@ sudo add-apt-repository ppa:nilarimogard/webupd8
 sudo add-apt-repository ppa:webupd8team/java
 sudo add-apt-repository ppa:webupd8team/sublime-text-3
 sudo add-apt-repository ppa:mc3man/trusty-media
+sudo add-apt-repository ppa:danielrichter2007/grub-customizer
 
 sudo apt-get update
 #Install Needed files to use and build amulet programs
-sudo apt-get install -y git build-essential subversion zsh oracle-java7-installer sublime-text-installer terminator unity-tweak-tool gstreamer0.10-ffmpeg gtk2-engines-murrine:i386 gtk2-engines-pixbuf:i386 sni-qt:i386 ubuntu-restricted-extras libdvdread4 libc6:i386 libx11-6:i386 libasound2:i386 libatk1.0-0:i386 libcairo2:i386 libcups2:i386 libdbus-glib-1-2:i386 libgconf-2-4:i386 libgdk-pixbuf2.0-0:i386 libgtk-3-0:i386 libice6:i386 libncurses5:i386 libsm6:i386 liborbit2:i386 libudev1:i386 libusb-0.1-4:i386 libstdc++6:i386 libxt6:i386 libxtst6:i386 libgnomeui-0:i386 libusb-1.0-0-dev:i386 libcanberra-gtk-module:i386 libxss1 libappindicator1 libindicator7 vlc
+sudo apt-get install -y git build-essential subversion zsh grub-customizer oracle-java7-installer sublime-text-installer terminator unity-tweak-tool gstreamer0.10-ffmpeg gtk2-engines-murrine:i386 gtk2-engines-pixbuf:i386 sni-qt:i386 ubuntu-restricted-extras libdvdread4 libc6:i386 libx11-6:i386 libasound2:i386 libatk1.0-0:i386 libcairo2:i386 libcups2:i386 libdbus-glib-1-2:i386 libgconf-2-4:i386 libgdk-pixbuf2.0-0:i386 libgtk-3-0:i386 libice6:i386 libncurses5:i386 libsm6:i386 liborbit2:i386 libudev1:i386 libusb-0.1-4:i386 libstdc++6:i386 libxt6:i386 libxtst6:i386 libgnomeui-0:i386 libusb-1.0-0-dev:i386 libcanberra-gtk-module:i386 libxss1 libappindicator1 libindicator7 vlc
 
 
 TI_MSPGCC_URL=http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSPGCC/2_01_01_00/exports/msp430-gcc-full-linux-installer-2.1.1.0.run
